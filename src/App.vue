8400f6409182e81a0a75115a67e68dcaff8ba493
nihad1213 <nihad.nemetli@gmail.com>
2025-11-15 16:17:59 +0400

    tailwind error fixed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SiteNavigation from './components/SiteNavigation.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SiteNavigation
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                "weather-primary": "#00668A",
-                "weather-secondary": "#004E71",
-            }
-        },
-        fontFamily: {
-            sans: ['Roboto', 'sans-serif'],
-        },
-        container: {
-            padding: "2rem",
-            center: true,
-        },
-        screens: {
-            'sm': '640px',
-            'md': '768px',
-    },   
-    plugins: [],
-    }
+  content: [
+    "./public/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      }
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
 }
